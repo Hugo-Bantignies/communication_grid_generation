10a8838e76d5f23ec1a2dc3a5c632dea54f189a4
Hugo-Bantignies <bantignieshugo@hotmail.fr>
2022-05-31 16:12:02 +0200

    New jupyter for the evaluation

--- podd_eval.py
+++
@@ -1,9 +0,0 @@
-from PictogramGrid import Pictogram,Page,Grid
-from EvaluationGrid import grid_distance_cost,sentence_distance_cost,grid_cost
-import os
-
-podd = Grid("podd.csv")
-tcof = Grid("default.csv")
-
-print("PODD : ",sentence_distance_cost(podd,"hier chat vouloir souris".split(" ")))
-print("TCOF : ",sentence_distance_cost(tcof,"hier chat vouloir souris".split(" ")))
